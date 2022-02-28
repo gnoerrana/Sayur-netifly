004cd74d2e9658b68b1dfe25ddf3c3b44e96f31a
Gilang <gnoerrana@gmail.com>
2022-03-01 00:31:55 +0700

    remove unused module, modif netlify.toml

--- src/components/CardItem/Templates/Peoples.tsx
+++ src/components/CardItem/Templates/Peoples.tsx
@@ -1,24 +1,12 @@
-import React, { useState } from "react";
+import React from "react";
 import { useQuery, gql } from "@apollo/client";
 import Grid from "@mui/material/Grid";
 import Card from "@mui/material/Card";
-import Paper from "@mui/material/Paper";
 import Box from "@mui/material/Box";
-import { styled } from "@mui/material/styles";
 import CardContent from "@mui/material/CardContent";
-import CardMedia from "@mui/material/CardMedia";
 import Typography from "@mui/material/Typography";
-import CardActions from "@mui/material/CardActions";
 import { CardActionArea } from "@mui/material";
 import CircularProgress from "@mui/material/CircularProgress";
-
-const Item = styled(Paper)(({ theme }) => ({
-	backgroundColor: theme.palette.mode === "dark" ? "#1A2027" : "#fff",
-	...theme.typography.body2,
-	padding: theme.spacing(1),
-	textAlign: "center",
-	color: theme.palette.text.secondary,
-}));
 
 const fetchData = (fetchData: any) => {
 	return gql`
